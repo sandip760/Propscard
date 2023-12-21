2a031f42baf35e9522ce7457cbc5302c4eee437e
sandip760 <133910320+sandip760@users.noreply.github.com>
2023-12-21 19:31:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {Products} from './component/product';
+ import contents from './contect';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+ export default function App() {
+     return(
+            <div className='App'>
+                {contents.map(contents => (
+                    <Products 
+                        key={contents.id}
+                        image={contents.image}
+                        name={contents.name}
+                        price={contents.price}
+                        totalSales={contents.totalSales}
+                        timeLeft={contents.timeLeft}
+                        rating={contents.rating}
+                    />
+                ))}
+            </div>
+     )
+ }
